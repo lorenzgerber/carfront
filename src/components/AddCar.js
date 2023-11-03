4e44b72686279f06a90e28d7083533eb357b341f
Lorenz Gerber <lorenz.gerber@lablicate.com>
2023-11-03 13:57:07 +0100

    including delete functionality

---
+++ src/components/AddCar.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material';
+import DialogTitle from '@mui/material';
+
+function AddCar (props) {
+    return (
+        <div></div>
+    );
+}
+
+export default AddCar;
--- src/components/Carlist.js
+++ src/components/Carlist.js
@@ -1,34 +1,72 @@
 import React, { useState, useEffect } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import Snackbar from '@mui/material/Snackbar';
+import { SERVER_URL } from '../constants';
 
 function Carlist() {
 
     const [cars, setCars] = useState([]);
+    const [open, setOpen] = useState(false);
+
+    const columns = [
+        { field: 'brand', headerName: 'Brand', width: 200 },
+        { field: 'model', headerName: 'Model', width: 200 },
+        { field: 'color', headerName: 'Color', width: 200 },
+        { field: 'year', headerName: 'Year', width: 150 },
+        { field: 'price', headerName: 'Price', width: 150},
+        {
+            field: '_links.self.href',
+            headerName: '',
+            sortable: false,
+            filterable: false,
+            renderCell: row => 
+                <button
+                    onClick={ () => onDelClick(row.id) }>Delete
+                </button>
+        }
+    ];
 
     useEffect(() => {
-        fetch('http://localhost:8080/api/cars')
+        fetchCars();
+    }, []);
+
+    const fetchCars = () => {
+        fetch(SERVER_URL + 'api/cars')
         .then(response => response.json())
         .then(data => setCars(data._embedded.cars))
         .catch(err => console.error(err));
-    }, []);
+    }
+
+    const onDelClick = (url) => {
+        if (window.confirm("Are you sure?")){
+            fetch(url, {method: 'DELETE'})
+        .then(response => {
+            if (response.ok){
+                fetchCars();
+                setOpen(true);
+            } else { 
+                alert('Something went wrong!');
+            } 
+        })
+        .catch(err => console.error(err))
+        }
+        
+    }
 
     return(
-        <div>
-            <table>
-                <tbody>
-                    {
-                        cars.map((car, index) =>
-                            <tr key={index}>
-                                <td>{car.brand}</td>
-                                <td>{car.model}</td>
-                                <td>{car.color}</td>
-                                <td>{car.year}</td>
-                                <td>{car.price}</td>
-                            </tr>
-                        )
-                    }
-                </tbody>
-            </table>
-        </div>
+       <div style={{ height: 500, width: '100%' }}>
+        <DataGrid
+            rows={cars}
+            columns={columns}
+            disableRowSelectionOnClick={true}
+            getRowId={row => row._links.self.href}/>
+        <Snackbar
+            open={open}
+            autoHideDuration={2000}
+            onClose={ () => setOpen(false)}
+            message="Car Deleted"
+        />
+       </div>
     );
 }
 
