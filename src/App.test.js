42320d0523afd312a4ae0dfbd7aadec2f8519588
Lorenz Gerber <lorenz.gerber@lablicate.com>
2023-11-08 11:54:09 +0100

    testing with jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import TestRenderer from 'react-test-renderer';
 import App from './App';
+import AddCar from './components/AddCar';
 
-test('renders learn react link', () => {
+
+test('open add car modal form', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  fireEvent.click(screen.getByText('New Car'));
+  expect(screen.getByRole('dialog')).toHaveTextContent('New Car');
+});
+
+test('renders a snapshot', () => {
+  const tree = TestRenderer.create(<AddCar />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
